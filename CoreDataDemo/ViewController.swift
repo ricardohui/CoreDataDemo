9070cfb4540635f09dc105188e120b3370b9d843
Ricardo Hui <eehaha@gmail.com>
2019-03-30 07:40:07 +0800

    Write and Retrieve from CoreData

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newUser = NSEntityDescription.insertNewObject(forEntityName:"Users", into: context)
+        newUser.setValue("kirsten",forKey:"username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(35, forKey: "age")
+        
+        do {
+            try context.save()
+            print("Saved")
+        } catch  {
+            print("There was an error")
+        }
+        
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do {
+        let results =  try context.fetch(request)
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    if let username = result.value(forKey: "username") as? String{
+                        print(username)
+                    }
+                }
+            }else{
+                print("No result")
+            }
+        } catch {
+            print("Couldn't fetch results")
+        }
     }
 
 
